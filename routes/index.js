21c3f4ccd6ef1fce36c37bd72adef391bd6218b0
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-05-10 18:06:20 +0200

    solved lab

--- routes/index.js
+++ routes/index.js
@@ -106,5 +106,20 @@ router.get('/profile', (req, res, next) => {
   res.render('profile')
 })
 
+//Protected routes
+
+const isLoggedIn = (req, res, next) => {
+  if (req.session.currentUser) next();
+  else res.redirect("/login");
+}
+
+router.get('/main', isLoggedIn, (req, res, next ) => {
+  res.render('main');
+})
+
+
+router.get('/private', isLoggedIn, (req, res, next ) => {
+  res.render('private');
+})
 
 module.exports = router;
